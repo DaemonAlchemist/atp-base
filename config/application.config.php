5cf36cbfb589911c791df6db2b0ff2d65238a60b
DaemonAlchemist <andy@wittrock.us>
2013-11-04 14:44:09 -0600

    Move application config back into application

---
+++ config/application.config.php
@@ -0,0 +1,27 @@
+<?php
+$env = getenv('APP_ENV') ?: 'development';
+
+return array(
+    'modules' => array(
+        'Application',
+		'Core'
+    ),
+
+    'module_listener_options' => array(
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+
+        'config_glob_paths' => array(
+            "config/autoload/{,*.}{global,{$env},local}.php",
+        ),
+
+		'config_cache_enabled' => ($env == 'production'),
+        'config_cache_key' => 'app_config',
+        'module_map_cache_enabled' => ($env == 'production'),
+        'module_map_cache_key' => 'module_map',
+        'cache_dir' => 'data/config/',
+        'check_dependencies' => ($env != 'production')
+    ),
+);
