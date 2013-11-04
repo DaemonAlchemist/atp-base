cbcdc73e3beab9321360f20864d3d8f291c303ba
DaemonAlchemist <andy@wittrock.us>
2013-11-04 00:07:54 -0600

    Turn on error reporting permanently for now

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
 chdir(dirname(__DIR__));
-include "vendor/ATP/Application/init.php";
+include "vendor/ATP/Application/init_zf2.php";
