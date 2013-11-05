19685222414fc8fa93ab3c7dbf9b2618aeaecda6
DaemonAlchemist <andy@wittrock.us>
2013-11-04 23:00:51 -0600

    Move default app options into ATP

--- config/application.config.php
+++ config/application.config.php
@@ -1,27 +1,9 @@
 <?php
-$env = getenv('APPLICATION_ENV') ?: 'development';
 
-return array(
+return \ATP\Application\Config::defaultOptions(array(
     'modules' => array(
         'Application',
 		'ATPCore'
-    ),
-
-    'module_listener_options' => array(
-        'module_paths' => array(
-            './module',
-            './vendor',
-        ),
-
-        'config_glob_paths' => array(
-            "config/autoload/{,*.}{global,{$env},local}.php",
-        ),
-
-		'config_cache_enabled' => ($env == 'production'),
-        'config_cache_key' => 'app_config',
-        'module_map_cache_enabled' => ($env == 'production'),
-        'module_map_cache_key' => 'module_map',
-        'cache_dir' => 'data/config/',
-        'check_dependencies' => ($env != 'production')
-    ),
+    )),
+	getenv('APPLICATION_ENV') ?: 'development'
 );
