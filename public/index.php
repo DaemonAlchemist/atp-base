8e0721d8cc8c72da3bae369f18613629623d1d6a
Andrew Wittrock <andy@wittrock.us>
2015-04-08 09:08:46 -0500

    Update index.php to load the new bootstrap

--- public/index.php
+++ public/index.php
@@ -3,4 +3,5 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 chdir(dirname(__DIR__));
-include "vendor/ATP/Application/init_zf2.php";
+require_once "vendor/autoload.php";
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
