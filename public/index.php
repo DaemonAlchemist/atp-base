268d31a9bfdb845f58809c40a4338d1493a8af90
Andrew Wittrock <andy@wittrock.us>
2015-04-08 09:20:27 -0500

    Set default timezone

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
+date_default_timezone_set('America/Chicago');
+
 chdir(dirname(__DIR__));
 require_once "vendor/autoload.php";
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
