caf06a4cb793ecf7d05517cbc11daef510f44d74
Andrew Wittrock <andy@wittrock.us>
2015-04-08 09:20:53 -0500

    Make sure Application is a valid module

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,9 @@ return \ATP\Application\Config::defaultOptions(array(
     'modules' => array(
         'Application',
 		'ATPCore',
+		'ATPAdmin',
+		'ATPCms',
+		'ATPGallery',
 		'AssetManager',
     )),
 	getenv('APPLICATION_ENV') ?: 'development'
---
+++ module/Application/src/Application/Module.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Application;
+
+class Module extends \ATP\Module
+{
+	protected $_moduleName = "Application";
+	protected $_moduleBaseDir = __DIR__;
+}
